bd4f5e5bf9199bbcc2b8fbdc7ad087063d249704
bron-lover <pjbasnet2006@gmail.com>
2026-03-05 18:44:53 -0800

    added unlocking for early return v2

--- hash-table-v2.c
+++ hash-table-v2.c
@@ -105,6 +105,14 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	if (list_entry != NULL)
 	{
 		list_entry->value = value;
+
+		// Unlock the appropriate mutex
+		error = pthread_mutex_unlock(&hash_table->mutexes[index]);
+		if (error != 0)
+		{
+			exit(error);
+		}
+
 		return;
 	}
 
